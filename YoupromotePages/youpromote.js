06652d276e8283dfd94b6fe966ed6c6922f6fab8
Oozyman2 <abdulhammedoluwashina@gmail.com>
2025-01-26 01:49:12 -0800

    first

---
+++ YoupromotePages/youpromote.js
@@ -0,0 +1,29 @@
+function menuToggle() {
+    const navLinks = document.querySelector('.nav-links')
+    navLinks.classList.toggle('active')
+}
+
+function dropdown() {
+      document.getElementById("myDropdown").classList.toggle('show')
+    
+
+    window.onclick = function(e) {
+        if (!e.target.matches('.dropbtw')) {
+            let myDropdown = document.getElementById("myDropdown");
+            if (myDropdown.classList.contains('show')) {
+                myDropdown.classList.remove('show')
+            };
+
+        }
+    }
+    
+}
+
+
+
+function closeNav() {
+    document.querySelector(".nav-links").classList.toggle("nav-hidden");
+    document.getElementById("LoginBtn").classList.toggle("activity")
+    document.getElementById("signUpBtn").classList.toggle("activity")  
+
+ }
